2453ee1e96512c78e52a44a587fa885dff20eca0
Santos Osmin Urrutia Iraheta <urrutia.osmin@gmail.com>
2020-04-10 21:36:01 -0600

    navbar ready

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bg-navbar shadow p-3">
+    <div class="md:mx-8 flex justify-between items-center">
+      <div class="text-default text-xl font-bold ">
+        Dashboard
+      </div>
+      <div class="hidden md:block w-1/3 p-1">
+        <input class="apparience-none rounded-lg bg-page w-full p-2 text-default" placeholder="Buscar.."> 
+      </div>
+      <div class="sm:flex sm:jutify-end text-default">
+          <div class="flex">
+            <div class=" text-xl p-2">
+              <i class="bx bx-moon"></i>
+            </div>
+            <div class="sm:hidden text-xl p-2">
+              <i class="bx bx-menu"></i>
+            </div>
+          </div>
+          <div class="hidden sm:flex p-2">
+            <div class="p-1">Inicio</div>
+            <div class="p-1">Inicio</div>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	theme: {
+		extend: {
+			colors: {
+				default: 'var(--default-text-color)',
+				main: 'var(--main-text-color)'
+			},
+			backgroundColor: {
+				page: 'var(--page-background-color)',
+				navbar: 'var(--navbar-background-color)',
+				'navbar-item':'var(--navbar-item-background-color)',
+				'navbar-item-hover': 'var(--navbar-item-hover-background-color)',
+				card: 'var(--card-background-color)',
+				button: 'var(--button-bakground-color)',
+				'button-hover': 'var(--button-hover-background-color)',
+			}
+		},
+	},
+	variants: {},
+	plugins: [],
+}
